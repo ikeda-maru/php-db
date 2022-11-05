2688321c40fcbbf60d92c45e951795325b4a1b34
atsushi-maru <ikeda.atsushi00@gmail.com>
2022-11-05 16:04:55 +0900

    delete.php

--- users.php
+++ users.php
@@ -39,6 +39,7 @@
       <th>年齢</th>
       <th>住所</th>
       <th>編集</th>
+      <th>削除</th>
 
     </tr>
     <?php
@@ -53,6 +54,7 @@
       <td>{$result['age']}</td>
       <td>{$result['address']}</td>
       <td><a href='update.php?id={$result['id']}'>編集</a></td>
+      <td><a href='delete.php?id={$result['id']}'>削除</a></td>
       </tr>
       ";
       echo $table_row;
